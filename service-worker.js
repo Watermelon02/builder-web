b874fce1e1afae12a61b601bc1a484e5cd1373da
Watermelon02 <1446157077@qq.com>
2025-10-04 12:21:07 +0800

    [fix]

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+// service-worker.js
+
+// 跳过等待，立即激活
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+// 激活时清理所有旧缓存
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((key) => caches.delete(key)))
+    )
+  );
+  clients.claim(); // 立即控制所有页面
+});
+
+// 所有请求都强制刷新缓存
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request, { cache: "reload" })
+      .catch(() => caches.match(event.request))
+  );
+});
