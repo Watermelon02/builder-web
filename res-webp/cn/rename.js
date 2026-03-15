630bf7a452c99dfa781787f9973fa1da20827759
Watermelon02 <1446157077@qq.com>
2026-03-15 11:28:43 +0800

    [+]替换为webp

---
+++ res-webp/cn/rename.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const path = require("path");
+
+// 修改为你的目标文件夹路径
+const folder = "./";
+
+fs.readdirSync(folder).forEach(file => {
+  if (file.toLowerCase().endsWith(".png")) {
+    // 提取 "LPA-01" 这样的前缀
+    const match = file.match(/^(LPA-\d+)/);
+    if (match) {
+      let baseName = match[1];
+      let ext = path.extname(file);
+      let newName = baseName + ext;
+      let oldPath = path.join(folder, file);
+      let newPath = path.join(folder, newName);
+
+      // 如果有重名文件，加序号
+      let counter = 2;
+      while (fs.existsSync(newPath)) {
+        newName = `${baseName}-${counter}${ext}`;
+        newPath = path.join(folder, newName);
+        counter++;
+      }
+
+      fs.renameSync(oldPath, newPath);
+      console.log(`重命名: ${file} -> ${newName}`);
+    }
+  }
+});
